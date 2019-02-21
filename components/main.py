ee270357a58f0d7318246d7e014aae32eb521895
Luis Abad <luisabad@Luiss-MBP.T-mobile.com>
2019-02-21 10:24:04 -0500

    adding background image

---
+++ components/main.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+background_image = pygame.image.load('../images/background.png')
+
+def main():
+    width = 1500
+    height = 1500
+    background_image
+
+
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Welcome to Pokimii')
+clock = pygame.time.Clock()
+
+
+
